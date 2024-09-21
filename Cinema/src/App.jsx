54ef282d09b1ebcd2abd63f2eecc4cfd377c63e1
unknown <tomdelintepro@gmail.com>
2024-09-21 11:21:03 +0200

    update

--- Cinema/src/App.jsx
+++ Cinema/src/App.jsx
@@ -8,9 +8,12 @@ function App() {
   return (
     <>
 <Header />
-
-    <main className='container mx-auto p-4'> 
-      <h1>sdsdsd</h1>
+    <main className='container mx-auto p-4 flex items-center justify-center'>
+      <div className='flex flex-col items-center text-center'>
+      <img src='https://image.noelshack.com/fichiers/2024/38/5/1726833341-test.jpg' alt=''></img>
+      <h1 className='text-white mt-9'>Jujutsu Kaizen</h1>
+      <span className='text-white mt-3'>l'auteur</span>
+      </div>
     </main>
     </>
   )
