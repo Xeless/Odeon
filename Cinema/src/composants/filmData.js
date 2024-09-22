76029d4a50d3c5d11e1942bcd6c89a2313d1fafa
Xeless <catxnero@outlook.fr>
2024-09-22 22:38:20 +0200

    update

--- Cinema/src/composants/filmData.js
+++ Cinema/src/composants/filmData.js
@@ -39,33 +39,33 @@ export const moviesData = {
   ],
   popular: [
     {
-      title: "Interstellar",
-      image: "https://fr.web.img6.acsta.net/pictures/14/09/24/12/08/158828.jpg",
+      title: "Jujutsu Kaisen 0",
+      image: "https://assets.kompasiana.com/items/album/2022/03/16/jjk-623142937a36cd140b0cbb22.jpg?t=o&v=300",
       author: "Christopher Nolan",
     },
     {
-      title: "Inception",
-      image: "https://m.media-amazon.com/images/M/MV5BMjExMjkwNTQ0Nl5BMl5BanBnXkFtZTcwNTY0OTk1Mw@@._V1_.jpg",
+      title: "Crow",
+      image: "https://fr.web.img6.acsta.net/img/13/09/1309072197382c48f58047238f9f44a7.jpg",
       author: "Christopher Nolan",
     },
     {
-      title: "Dune",
-      image: "https://fr.web.img4.acsta.net/pictures/24/01/26/10/18/5392835.jpg",
+      title: "Demon Slayer Le Train De L'infini",
+      image: "https://fr.web.img6.acsta.net/pictures/21/09/27/11/50/4294280.jpg",
       author: "Denis Villeneuve",
     },
     {
-      title: "Babylon",
-      image: "https://toutelaculture.com/wp-content/uploads/2023/01/affiche-Babylone-680x924.webp",
+      title: "Fast & Furious X",
+      image: "https://imgsrc.cineserie.com/2023/03/2356036.jpg?ver=1",
       author: "Damien Chazelle",
     },
     {
-      title: "Legende",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCzuOPfSrDpaY3Gne09dC43dTHWdzeSlmShg&s",
+      title: "Avatar",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeop6ezR8m0rui3WB1Ijh8pwV_52lFOm-m5ocjeRl6FTt69i1e2Krm_UBYXxzG3aHSXRc&usqp=CAU",
       author: "Brian Helgeland",
     },
     {
-      title: "Chocolat",
-      image: "https://belgik-mojaik.be/media/film-affiche-chocolat.jpg",
+      title: "Joker",
+      image: "https://media.senscritique.com/media/000022304534/300/joker_folie_a_deux.png",
       author: "Roschdy Zem",
     },
     {
--- Cinema/src/pages/Login.jsx
+++ Cinema/src/pages/Login.jsx
@@ -1,26 +1,57 @@
 const Login = () => {
     return (
-        <div className="flex justify-center items-center mt-96 bg-slate-500">
-            <form>
-                <label for="email" className="block text-gray-300">E-mail</label>
-                <input 
-                type="email"
-                 id="email"
-                  className="" 
-                  placeholder="entre votre mail"
-                />
-                <label for="password" className="block text-white">Password</label>
-                <input 
-                type="password"
-                id="password"
-                className=""
-                placeholder="entre votre password" 
-                 />
-            </form>
-
-        </div>
-
-    )
-}
-
-export default Login
+      <div
+        className="min-h-screen flex flex-col justify-center items-center bg-slate-500"
+        style={{
+          backgroundImage:
+            'url("https://ds.static.rtbf.be/article/image/770x433/1/9/0/6e0917469214d8fbd8c517dcdc6b8dcf-1630402493.jpg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <form className="bg-opacity-10 backdrop-blur-lg p-8 rounded-xl border bg-purple-900 max-w-sm w-full">
+          <h1 className="text-white text-3xl font-bold text-center mb-6">Login</h1>
+          
+          <label htmlFor="email" className="block mb-2 text-white">
+            E-mail
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="p-2 rounded-md w-full mb-4"
+            placeholder="username@gmail.com"
+          />
+          
+          <label htmlFor="password" className="block mb-2 text-white">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="p-2 rounded-md w-full mb-6"
+            placeholder="Password"
+          />
+          
+          <button className="text-white p-3 rounded-md w-full bg-purple-700">
+            Sign In
+          </button>
+          
+          <span className="flex text-white font-bold justify-center mt-5">
+            ou continue avec
+          </span>
+          
+          <div className="flex gap-4 justify-center mt-5">
+            <button className="w-36 flex justify-center p-3 bg-white rounded-lg">
+              <img src="../../public/uploads/google.svg" alt="Google" />
+            </button>
+            <button className="w-36 flex justify-center p-3 bg-white rounded-lg">
+              <img src="../../public/uploads/facebook.svg" alt="Facebook" />
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
